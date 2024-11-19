e0dd9733260de02383f0782459c0ee0f08d09d76
artc0des <art.c@hey.com>
2024-11-19 00:14:54 -0600

    Picking color schemes is hard

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,9 +14,17 @@ module.exports = {
         lightBlue: 'hsl(233, 100%, 69%)',
         darkGrayishBlue: 'hsl(210, 10%, 33%)',
         grayishBlue: 'hsl(201, 11%, 66%)',
+        artBackground: 'hsl(200, 13.43%, 13.14%)',
+        artHeader: 'hsl(203.48, 25%, 36.08%)',
+        artTextColor: 'hsl(39.47, 89.06%, 74.9%)',
+        artHighlight: 'hsl(39.06, 94.38%, 48.82%)',
+        artBorder: 'hsl(204, 14.29%, 6.86%)',
       },
       fontFamily: {
-        sans: ['Bai Jamjuree', 'sans-serif'],
+        sans: ['Josefin Sans', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+        alata: ['Alata'],
+        nothing: ['Nothing You Could Do']
       },
     },
   },
