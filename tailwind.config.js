9d9d2c2294722c27ce66227827d60188ad5d41fe
artc0des <art.c@hey.com>
2024-11-20 22:58:12 -0600

    separating project

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,13 @@ module.exports = {
         artTextColor: 'hsl(39.47, 89.06%, 74.9%)',
         artHighlight: 'hsl(39.06, 94.38%, 48.82%)',
         artBorder: 'hsl(204, 14.29%, 6.86%)',
+        cyan: 'hsl(180, 66%, 49%)',
+        cyanLight: 'hsl(180, 66%, 69%)',
+        darkViolet: 'hsl(257, 27%, 26%)',
+        red: 'hsl(0, 87%, 67%)',
+        grayishViolet: 'hsl(257, 7%, 63%)',
+        veryDarkBlue: 'hsl(255, 11%, 22%)',
+        veryDarkViolet: 'hsl(260, 8%, 14%) ',
       },
       fontFamily: {
         sans: ['Josefin Sans', 'sans-serif'],
