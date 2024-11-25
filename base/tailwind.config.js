4747c113df49877d217bb52b4d16f0ae15f677d8
artc0des <art.c@hey.com>
2024-11-24 22:18:46 -0600

    light/dark demo

---
+++ base/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+      },
+      fontFamily: {
+      },
+      spacing: {
+      },
+    },
+  },
+  plugins: [],
+}
---
+++ testimonial/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['./*.html'],
+  screens: {
+    sm: '480px',
+    md: '768px',
+    lg: '976px',
+    xl: '1440px',
+  },
+  theme: {
+    extend: {
+      colors: {
+      },
+      fontFamily: {
+        sans: ['Barlow Semi Condensed', 'sans-serif'],
+      },
+      spacing: {
+      },
+    },
+  },
+  plugins: [],
+}
